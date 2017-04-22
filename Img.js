d92b9b8f0a48e900e6de918db03568a18c1a8b7d
Gilad <giladaya@gmail.com>
2017-04-22 11:44:36 +0300

    initial, working

---
+++ Img.js
@@ -0,0 +1,23 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['b'], function (b) {
+            // Also create a global in case some scripts
+            // that are loaded still are looking for
+            // a global even when an AMD loader is in use.
+            return (root.Img = factory(b));
+        });
+    } else {
+        // Browser globals
+        root.Img = factory(root.b);
+    }
+}(this, function (b) {
+    //use b in some fashion.
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return {
+        log: function(msg) { console.log(msg) }
+    };
+}));
